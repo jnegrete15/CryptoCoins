3456cd09f2fa2164b3d2eb5085c94dbf27f3f7c2
Ale Figueroa <afigueroa@goamv.com>
2019-11-26 08:49:18 -0600

    First commit

---
+++ lib/crypto_coins/api.ex
@@ -0,0 +1,14 @@
+defmodule Api.CryptoCoins do
+  # Check Tesla Docs
+  # API Base is: "https://api.coinranking.com/v1/public"
+  # Endpoint is: /coins
+
+  # returns {:ok, array_of_coins}
+  def coins() do
+  end
+
+  # returns array_of_coins
+  #raise if error
+  def coins!() do
+  end
+end
---
+++ test/crypto_coins_test.exs
@@ -0,0 +1,8 @@
+defmodule CryptoCoinsTest do
+  use ExUnit.Case
+  doctest CryptoCoins
+
+  test "greets the world" do
+    assert CryptoCoins.hello() == :world
+  end
+end
