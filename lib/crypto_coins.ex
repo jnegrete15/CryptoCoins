06eea9bd0ef665e2d03f6b922cefedb5fb7be674
Jose Alberto Negrete <jnegrete15@ucol.mx>
2019-12-04 13:19:23 -0600

    Finalización de los métodos

--- lib/crypto_coins.ex
+++ lib/crypto_coins.ex
@@ -1,16 +1,34 @@
 defmodule CryptoCoins do
+
+  require CryptoCoins.Api
+
   def coin(coins, name) when is_binary(name) do
+    #Esta función compara el nombre de la moneda que le pasamos. Nos regresa la moneda cuando el nombre
+    #coincide con el nombre de la moneda que le pasamos
+    Enum.find(coins, fn coin -> Map.fetch!(coin, "name") == name end)
   end
 
   def most_valuable(coins) do
+    #Aquí usamos la función max_by y de parámetro le decimos que sea por el precio.
+    Enum.max_by(coins, fn coin -> Float.parse(Map.fetch!(coin, "price")) end)
   end
 
   def most_capped(coins) do
+    #Esta función es igual que la anterior pero en este caso en lugar de pasarle el price le ponemos
+    #marketCap,
+    Enum.max_by(coins, fn coin -> Float.parse(Map.fetch!(coin, "marketCap")) end)
   end
 
   def most_ranked(coins) do
+  #Aqui como la moneda más rankeada (la número 1), entonces usamos la funcion Enum.min. Para que
+  #nos jale el mínimo.
+    Enum.min_by(coins, fn coin -> Float.parse(Map.fetch!(coin, "rank")) end)
   end
 
+    #Usamos la función sort by, le pasamos el array, y el parámatero por el que lo queremos ordenar
+
+
   def sort_by(coins, field) do
+    Enum.sort_by(coins, fn coin -> Map.fetch!(coin , field) end)
   end
 end
--- lib/crypto_coins/api.ex
+++ lib/crypto_coins/api.ex
@@ -1,14 +1,39 @@
-defmodule Api.CryptoCoins do
+defmodule CryptoCoins.Api do
   # Check Tesla Docs
   # API Base is: "https://api.coinranking.com/v1/public"
   # Endpoint is: /coins
 
+
   # returns {:ok, array_of_coins}
+    use Tesla
+
+    plug Tesla.Middleware.BaseUrl, "https://api.coinranking.com/v1/public"
+    plug Tesla.Middleware.Headers, [{"", ""}]
+    plug Tesla.Middleware.JSON
+
+
   def coins() do
+    {:ok, response} = get("/coins")
+    if(response.status in 200..299) do
+      coins = response |> Map.fetch!(:body) |> Map.fetch!("data") |> Map.fetch!("coins")
+    end
+#    if ok == :ok do
+#      if status in 200..299 do
+
+#        {:ok, coins}
+#      else
+#        {:error, env}
+#      end
+#    else
+#      {:error, response}
+#    end
   end
 
   # returns array_of_coins
   #raise if error
   def coins!() do
+    {:ok, response} = get("/coins")
+    raise "Algo salió mal"
   end
+
 end
